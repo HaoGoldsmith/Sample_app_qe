d9e17fdef699bc1c9b635fbc13cccb1e310ca54d
HaoGokdsmith <saya0304@yandex.ru>
2023-01-13 12:24:54 +0300

    test changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
     test "name should not be too long" do
     @user.name = "a" * 51
@@ -9,6 +10,15 @@ class UserTest < ActiveSupport::TestCase
   end
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 end
