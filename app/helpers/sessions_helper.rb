4871968942acdf1229409556e7b25293e912a39c
HaoGokdsmith <saya0304@yandex.ru>
2023-01-16 14:08:44 +0300

    test - вот тут пошло не так с тестами, т.к я пропустила солидный кусок

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,36 @@
 module SessionsHelper
   # Logs in the given user.
   def log_in(user)
-    session[:user_id] = user.id
+    session[:user_id] = user.id #добавляет в дикт(хэш) сессия параметр юзер_ид равный идентификатору юзера
   end
 
-  # Returns the current logged-in user (if any).
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id]) #если юзер_ид это ид, взятый из сессии
+      @current_user ||= User.find_by(id: user_id) #то текущий юзер либо тот,что сейчас, либо им становится найденный по ид
+    elsif (user_id = cookies.encrypted[:user_id]) #если юзер_ид это ид, взятый из куков(зашифрованный ид),
+      # т.е сессия не открыта, но когда-то была создана
+      user = User.find_by(id: user_id) #ищем юзера по этому ид
+      if user && user.authenticated?(cookies[:remember_token]) #если нашли его и у него авторизовался ремембер токен
+        log_in user #логинимся и делаем текущим юзером
+        @current_user = user
+      end
     end
   end
 
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember #используется функция ремембер класса юзер. запоминаем его
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   def logged_in?
     !current_user.nil?
@@ -18,6 +38,7 @@ module SessionsHelper
 
   # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -6,5 +7,39 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password, presence: true, length: { minimum: 6 }
+
+
+  # Returns the hash digest of the given string. закриптованный новый пароль с ценой кост(встроенно)
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+
+  # Returns a random token. Создает защищенный рандомный токен 22 символа (64 вариации)с помощью встроенная функция
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+
+  # Remembers a user in the database for use in persistent sessions. учебник говорит, селф перед переменной - к тому,
+  # чтобы переменная не была локальной
+  def remember
+    self.remember_token = User.new_token #присваиваем классу параметр ремембер_токен, который равен созданному ф-ей
+    # класса новому токену
+      update_attribute(:remember_digest, User.digest(remember_token)) #заменяем в текущего юзера параметр
+  #   ремембер_дайджест со значением созданным ф-ей юзер.дайджест на основе ремембер токена
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
